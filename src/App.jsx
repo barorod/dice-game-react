358809d6a50f26c04a7492d39d042802081bf36c
Barorod <128448506+barorod@users.noreply.github.com>
2024-02-06 13:15:59 +0800

    Add luckyn state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Lucky7 from './Lucky7';
 import Die from './Die';
 import Dice from './Dice';
+import LuckyN from './LuckyN';
 
 function App() {
 	return (
 		<>
-			<Dice dice={[3, 6, 1]} />
-			<Dice dice={[1, 2, 3]} color="red" />
+			<LuckyN />
+			{/* <Dice dice={[3, 6, 1]} /> */}
+			{/* <Dice dice={[1, 2, 3]} color="red" /> */}
 		</>
 	);
 }
--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -1,3 +1,13 @@
-export default fuction LuckyN({numDice, goal}){
-    return
+import { useState } from 'react';
+import { getRolls } from './utils';
+import Dice from './Dice';
+
+export default function LuckyN({ numDice = 2, goal = 7 }) {
+	const [dice, setDice] = useState(getRolls(numDice));
+	return (
+		<main className="LuckyN">
+			<h1>Lucky {goal}</h1>
+			<Dice dice={dice} />
+		</main>
+	);
 }
